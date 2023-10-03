f4d74a0b24c90b879f4bd9902f8839939383e6b2
Andresknaz <andrescanaveral.p@gmail.com>
2023-10-02 23:59:14 -0500

    create CambioMoneda

---
+++ cambio-de-monedas/src/CambioMoneda.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+function CambioMoneda({ tipo, onCambio }){
+    const [cantidad, setCantidad] = useState('');
+
+    const handleChange = (e) => {
+        setCantidad(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onCambio(parseFloat(cantidad));
+    };
+
+    return (
+        <div>
+            <h2>Cambio a {tipo}</h2>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Cuanta cantidad de {tipo} vas a cambiar:
+                    <input type="number" step="0.01" value={cantidad} onChange={handleChange} />
+                </label>
+                <button type="submit">Calcular</button>
+            </form>
+        </div>
+    );
+}
+
+export default CambioMoneda;
