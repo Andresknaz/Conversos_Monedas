d7e89d1836067dc34006f7ecb7f15cc4bdbd456c
Andresknaz <andrescanaveral.p@gmail.com>
2023-10-05 13:25:23 -0500

    Update App.js

--- cambio-de-monedas/src/App.js
+++ cambio-de-monedas/src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Menu from './Menu';
+import CambioMoneda from './CambioMoneda';
+import Resultado from './Resultado';
+
+const DolarPeso = 4.348;
 
 function App() {
+  const [opcion, setOpcion] = useState(null);
+  const [resultado, setResultado] = useState(null);
+  const [tipoMoneda, setTipoMoneda] = useState('');
+  const [cantidad, setCantidad] = useState(null); // Agrega cantidad al estado
+
+  const cambiarDolar = (dolares) => {
+    const pesosC = dolares * DolarPeso;
+    return pesosC;
+  };
+
+  const cambiarPesos = (pesos) => {
+    const dolares = pesos / DolarPeso;
+    return dolares;
+  };
+
+  const solicitarCantidad = (tipo) => {
+    const cantidad = parseFloat(prompt(`Cuanta cantidad de ${tipo} vas a cambiar:`));
+    setCantidad(cantidad); // Actualiza el estado de cantidad
+    return cantidad;
+  };
+
+  const handleOptionSelect = (selectedOption) => {
+    setOpcion(selectedOption);
+    setResultado(null);
+    setTipoMoneda(selectedOption === 1 ? 'Pesos Colombianos' : 'Dolares');
+  };
+
+  const handleCambio = (cantidad) => {
+    if (opcion === 1) {
+      const pesos = cambiarPesos(cantidad);
+      setResultado(pesos);
+    } else if (opcion === 2) {
+      const dolares = cambiarDolar(cantidad);
+      setResultado(dolares);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {opcion === null ? (
+        <Menu onSelectOption={handleOptionSelect} />
+      ) : (
+        <>
+          <CambioMoneda tipo={tipoMoneda} onCambio={handleCambio} />
+          {resultado !== null && (
+            <Resultado cantidad={cantidad} tipo={tipoMoneda.toLowerCase()} resultado={resultado} />
+          )}
+        </>
+      )}
     </div>
   );
 }
